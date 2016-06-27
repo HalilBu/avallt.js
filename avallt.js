534dcada084b0da20adb36b2cdb916a5ae9c5ace
Halil Bugdayci <dev@halilbu.de>
2016-06-27 20:04:51 +0200

    Start implementing function diff

--- avallt.js
+++ avallt.js
@@ -3,4 +3,20 @@
     var addFn = function(fnName, fn) {
         window.avalltJS[fnName] = fn;
     }
+    var diff = function(object1, object2) {
+        var propertiesOf1 = [],
+            propertiesOf2 = [];
+
+        for (var property in object1) {
+            if (object1.hasOwnProperty(property)) {
+                propertiesOf1.push(property);
+            }
+        }
+
+        for (var property in object2) {
+            if (object2.hasOwnProperty(property)) {
+                propertiesOf2.push(property);
+            }
+        }
+    }
 })()
